7f8cc0abe1abd4bea39cc0e423d8fe33183c0ab3
luhluh-17 <castro.ralph17@gmail.com>
2023-05-01 15:02:20 +0800

    add post moel

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@
 # Users can sign up, log in, and perform various actions within the app.
 # The User model validates the presence of both the email and password fields.
 class User < ApplicationRecord
+  has_many :posts
+
   validates :email, presence: true
   validates :password, presence: true
 end
